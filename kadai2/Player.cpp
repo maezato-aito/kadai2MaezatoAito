ae54849e6432cd1b9109df881dd18c8c3a7d133c
Your Name <aito-maezato-k22f@stu.kbc.ac.jp>
2023-08-18 15:04:01 +0900

    a

--- kadai2/Player.cpp
+++ kadai2/Player.cpp
@@ -28,24 +28,29 @@ void Player::Update()
 	//アナログスティックの入力の受け取り
 	Get_MoveX = Input::Get_AnlogSticX();
 	Get_MoveY = Input::Get_AnlogSticY();
+	if (WINDOW_X <= locationX <= WINDOW_WIDTH && WINDOW_Y <= locationY <= WINDOW_HEIGHT) 
+	{
+		//プレイヤーの横移動(左)
+		if (Get_MoveX <= -1)
+		{
+			locationX -= speed;
+		}
+		//プレイヤーの横移動(右)
+		else if (1 <= Get_MoveX)
+		{
+			locationX += speed;
+		}
 
-	//プレイヤーの横移動
-	if (Get_MoveX <= -1)
-	{
-		locationX -= speed;
-	}
-	else if (Get_MoveX >= 1)
-	{
-		locationX += speed;
-	}
-	//プレイヤーの縦の移動
-	if (Get_MoveY <= -1)
-	{
-		locationY -= speed;
-	}
-	else if (Get_MoveY >= 1)
-	{
-		locationY += speed;
+		//プレイヤーの縦の移動(上)
+		if (Get_MoveY <= -1)
+		{
+			locationY -= speed;
+		}
+		//プレイヤーの縦の移動(下)
+		else if (Get_MoveY >= 1)
+		{
+			locationY += speed;
+		}
 	}
 }
 
@@ -56,6 +61,9 @@ void Player::Draw()
 	//傾き描画
 	DrawFormatString(0, 0, 0xffffff, "%d", Get_MoveX, TRUE);
 	DrawFormatString(0, 40, 0xffffff, "%d", Get_MoveY, TRUE);
+
+	DrawFormatString(0, 80, 0xffffff, "%d", locationX, TRUE);
+	DrawFormatString(0, 120, 0xffffff, "%d", locationY, TRUE);
 
 }
 
--- kadai2/comon.h
+++ kadai2/comon.h
@@ -2,6 +2,8 @@
 #include"DxLib.h"
 
 //âÊñ ÉTÉCÉY
+#define WINDOW_Y (0)
 #define WINDOW_HEIGHT (720)
+#define WINDOW_X (0)
 #define WINDOW_WIDTH (1280)
 #define WINDOW_BIT (32)
