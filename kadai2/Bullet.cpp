e6a1f0de389950e041fcde965c5d237d07db600f
Your Name <aito-maezato-k22f@stu.kbc.ac.jp>
2023-08-31 15:05:35 +0900

    a

--- kadai2/Bullet.cpp
+++ kadai2/Bullet.cpp
@@ -5,7 +5,7 @@ Bullet::Bullet(int x, int y)
 	locationX=x;//X座標
 	locationY=y;//Y座標
 
-	Bulletsflg = 0;
+	Bulletsflg = 1;
 }
 Bullet::~Bullet()
 {
@@ -14,19 +14,23 @@ Bullet::~Bullet()
 void Bullet::Update()
 {
 	//弾の座標移動
-	if (WINDOW_X < locationX < WINDOW_WIDTH)
+	if ((WINDOW_X < locationX < WINDOW_WIDTH) && Bulletsflg==1)
 	{
 		locationX += 20;
 	}
 	else
 	{
-
+		//画面買いならバレットフラグを0にする。
+		Bulletsflg = 0;
 	}
 }
 void Bullet::Draw()const
 {
 	//弾の描画
 	DrawCircle(locationX, locationY,10,0xf12fff, TRUE);
+
+	//弾のフラグの描画
+	DrawFormatString(0, 400, 0xffffff, "%d", Bulletsflg, TRUE);
 
 }
 void Bullet::GetDamage()
